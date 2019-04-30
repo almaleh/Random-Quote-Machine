ab197cdfecf63e62c0b3a0d661a86644cf0027d0
BesherAlMaleh <besher.m@gmail.com>
2019-04-29 20:41:19 -0400

    connect redux to react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider, connect } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware} from 'redux'
 
-function App() {
-  return (
-    <div className="App">
+// Redux
+const ADD_QUOTE ='ADD_QUOTE'; 
+const quotesReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_QUOTE:
+      return [...state, action.quote]; 
+      default: 
+      return state; 
+  }
+}
+
+const addQuote = (quote) => {
+  return {
+    type: ADD_QUOTE,
+    quote
+  }
+}
+
+const store = createStore(quotesReducer); 
+
+// React-Redux
+
+const mapStateToProps = (state) => {
+  return { quotes: state }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewQuote: (quote) => {
+      dispatch(addQuote(quote));
+    }
+  }
+}
+
+// React
+
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ""
+    }
+    this.submitQuote = this.submitQuote.bind(this); 
+  }
+
+  submitQuote() {
+    this.props.submitNewQuote("This is a quote")
+  }
+
+  render() {
+    let quotes = this.props.quotes.map( (quote, index) => {
+      return (
+        <p key={index}>{quote}</p>
+      )
+      })
+    console.log(this.props.quotes)
+    return(
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
+    )
+  }
+}
+
+
+function App() {
+  return (        
+    <Provider store={store}>
+      <Container/>
+    </Provider>
   );
 }
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational)
+
+
 
 export default App;
